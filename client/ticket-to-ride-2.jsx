057ee6bb928a0751a479f200f6d8fc6f90956046
Copons <copons@users.noreply.github.com>
2018-01-09 18:56:30 +0000

    Update eslint

--- client/ticket-to-ride-2.jsx
+++ client/ticket-to-ride-2.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { hot } from 'react-hot-loader';
 import './style.scss';
 
-const TicketToRide2 = () => (
+export const TicketToRide2 = () => (
 	<div>
 		<strong>TTR2</strong>
 	</div>
